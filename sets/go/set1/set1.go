060374650f453bf0acdc36c375368360d9f5cf24
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-05-24 05:39:18 -0500

    Completes S2Q1 in go

--- sets/go/set1/set1.go
+++ sets/go/set1/set1.go
@@ -1,64 +1,83 @@
 package set1
 
 import (
-	"fmt"
-	"math"
+	"errors"
 	"unicode"
 	"unicode/utf8"
 )
 
-// Converts a hex input string into its integer representation
-func ConvertHexStringToVal(s string) int {
+// GetBytesFromHex takes a string representation of a hex number and
+// returns an array of the corresponding bytes
+func GetBytesFromHex(s string) ([]byte, error) {
 	base16_map := map[rune]int{
 		'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
 		'6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11,
 		'C': 12, 'D': 13, 'E': 14, 'F': 15}
-	exponent := float64(len(s) - 1)
-	result := 0
-	for _, runeValue := range s {
-		result += base16_map[unicode.ToUpper(runeValue)] * int(math.Pow(16.0, exponent))
-		exponent -= 1.0
+	strLen := len(s)
+	if strLen%2 != 0 {
+		return make([]byte, 0), errors.New("Hex string must contain an" +
+			" even multiple of characters.")
 	}
-	return result
+	bytes := make([]byte, strLen/2)
+	strRunes := []rune(s)
+	for i := strLen; i > 0; i -= 2 {
+		curChars := strRunes[i-2 : i]
+		bytes[i/2-1] = byte(16*base16_map[unicode.ToUpper(curChars[0])] +
+			base16_map[unicode.ToUpper(curChars[1])])
+	}
+	return bytes, nil
+}
+
+// base64Pad returns the appropriate padding and padding size for a given input
+// string to be converted to base64
+func base64Pad(s string) (string, int) {
+	length := len(s)
+	padLength := 0
+	inputPad := ""
+	if length%3 == 1 {
+		padLength = 2
+		inputPad = "00"
+	} else if length%3 == 2 {
+		padLength = 1
+		inputPad = "0"
+	}
+	return inputPad, padLength
 }
 
 // HexToBase64 takes an input string that is a hex representation and returns
 // its base64 representation
-func HexToBase64(s string) string {
-	base64_chars := [64]rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
-		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-		'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-		'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-		'0', '+', '/'}
+func HexToBase64(s string) (string, error) {
+	//base64_chars := [64]rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+	//	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+	//	'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+	//	'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+	//	'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	//	'+', '/'}
 
+	const base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwx" +
+		"yz0123456789+/"
 	length := utf8.RuneCountInString(s)
-	// Pad input
-	padLength := 0
-	if length%3 == 1 {
-		s += "00"
-		length += 2
-		padLength = 2
-	} else if length%3 == 2 {
-		s += "0"
-		length += 1
-		padLength = 1
-	}
-	val := ConvertHexStringToVal(s)
-	// Construct base64 string
-	output := make([]rune, length/6)
-	if padLength == 2 {
-		output[length/6 - 1] = '='
-		output[length/6 - 2] = '='
-	} else if padLength == 1 {
-		output[length/6 - 1] = '='
-	}
-	for i := 0; i < length/6 - padLength; i++ {
-		currentBits := val & (2 << 6)
-		fmt.Printf("[%d]val: %d\tcurrentBits: %d\n",i, val, currentBits)
-		val = val >> 6
-		output[length/6-i-1] = base64_chars[currentBits]
-	}
-	return string(output)
-}
 
+	// Pad input
+	inputPad, padLength = base64Pad(s)
+
+	bytes, err := GetBytesFromHex(s + inputPad)
+	if err != nil {
+		return "", err
+	}
+
+	numBytes := len(bytes)
+	result := make([]rune, (length+padLength)*4/6)
+	byteIndex := len(result) - 1
+	for i := numBytes; i > 0; i -= 3 {
+		byteTriple := bytes[i-3 : i]
+		val := (2<<15)*int(byteTriple[0]) + (2<<7)*int(byteTriple[1]) +
+			int(byteTriple[2])
+		for j := 0; j < 4; j++ {
+			c := (val >> uint(6*j)) & 0x3F
+			result[byteIndex-j] = rune(base64_chars[c])
+		}
+		byteIndex -= 4
+	}
+	return string(result), nil
+}
--- sets/go/set1/set1_test.go
+++ sets/go/set1/set1_test.go
@@ -1,38 +1,51 @@
 package set1
 
 import (
+	"bytes"
 	"testing"
 )
 
-func TestHexToString(t *testing.T) {
-	if ConvertHexStringToVal("A") != 10 {
-		t.Error("Expected A converted to 10")
-	}
-
+func TestGetBytesFromHex(t *testing.T) {
 	base16_map := map[rune]int{
 		'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
 		'6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11,
 		'C': 12, 'D': 13, 'E': 14, 'F': 15}
 
 	for k, v := range base16_map {
-		if ConvertHexStringToVal(string(k)) != v {
-			t.Errorf("Expected %s to be converted to %d\n", k, v)
+		expected := make([]byte, 1)
+		expected[0] = byte(v)
+		actual, err := GetBytesFromHex("0" + string(k))
+		if err != nil {
+			t.Errorf("error - %s\n", err)
+		}
+		if !bytes.Equal(actual, expected) {
+			t.Errorf("Expected %v to be converted to %v. Got: %v\n",
+				k, expected, actual)
 		}
 	}
 
-	if ConvertHexStringToVal("12F") != 303 {
-		t.Errorf("Expected 12F to be converted to 303, Got: %d\n", ConvertHexStringToVal("12F"))
+	actual, err := GetBytesFromHex("012F")
+	if err != nil {
+		t.Errorf("error - %s\n", err)
+	}
+	expected := make([]byte, 2)
+	expected[0] = byte(1)
+	expected[1] = byte(47)
+	if !bytes.Equal(actual, expected) {
+		t.Errorf("Expected 12F to be converted to %v. Got: %v\n",
+			expected, actual)
 	}
 }
 
-func TestHextToBase64(t *testing.T) {
-	
-}
-
 func TestSet1Challenge1(t *testing.T) {
-	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-	actual := HexToBase64(input)
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b6520612" +
+		"0706f69736f6e6f7573206d757368726f6f6d"
+	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11" +
+		"c2hyb29t"
+	actual, err := HexToBase64(input)
+	if err != nil {
+		t.Errorf("error - %s\n", err)
+	}
 	if expected != actual {
 		t.Errorf("Attempted to convert %s to base64\n\tExpected: %s\n\tGot: %s\n",
 			input, expected, actual)
